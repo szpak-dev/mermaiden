64a9c8c8a3d8ba958f258e7deda3d4325e3e20b9
Tomasz Szpak <tomszp@gmail.com>
2026-08-06 17:20:07 +0200

    clear new_proto

--- src/new_proto/core/annotation.py
+++ src/new_proto/core/annotation.py
@@ -1,14 +1,1 @@
 from new_proto.interface import Interface
-
-
-class Annotatable(Interface):
-    """Marks a type that deliberately permits annotations to target its instances."""
-
-    pass
-
-
-class Annotation(Interface):
-    """Optional meaning that refers to explicitly annotatable targets without mutating them."""
-
-    @Interface.prop
-    def targets(self) -> tuple[Annotatable, ...]: ...
