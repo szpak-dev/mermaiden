b0071fdd4f1f3cb7126ea578fdd54fe5d7675e2c
Tomasz Szpak <tomszp@gmail.com>
2026-08-26 16:28:23 +0200

    Add directional C4 relationships (#32) (#40)

--- src/mermaiden/diagrams/c4/relations.py
+++ src/mermaiden/diagrams/c4/relations.py
@@ -1,6 +1,16 @@
 
+from enum import StrEnum
+
 from ...core.relation import Relation
 
 
+class RelationshipDirection(StrEnum):
+    DEFAULT = "Rel"
+    RIGHT = "Rel_R"
+    LEFT = "Rel_L"
+    UP = "Rel_Up"
+    DOWN = "Rel_Down"
+
+
 class Relationship(Relation):
-    pass
+    direction: RelationshipDirection = RelationshipDirection.DEFAULT
